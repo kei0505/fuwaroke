906346796a95c1bea4d5b2f40679cc44447c73cc
katuomirin <2003euph@gmail.com>
2024-05-15 11:55:16 +0900

    Update home.php

---
+++ System2/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        
+    </header>
+    <main>
+        
+    </main>
+    <footer>
+    </footer>
+</body>
+</html>
