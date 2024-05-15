5fb31fc92f91d51333ba3f3bd9d8669ec5b1cc37
Eula0313 <2201204@s.asojuku.ac.jp>
2024-05-15 10:24:22 +0900

    DB 接続

---
+++ db-connect.php
@@ -0,0 +1,8 @@
+<?php
+const SERVER = 'mysql305.phy.lolipop.lan';
+const DBNAME = 'LAA1517492-fuwaroke';
+const USER = 'LAA1517492';
+const PASS = 'Pass0313';
+
+$connect = 'mysql:host='.SERVER . ';dbname='. DBNAME . ';charaset=utf8';
+?>
